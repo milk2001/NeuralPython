c166401ffcc9798fa5b256fc4d32e048c8610013
Matteo <matteo@matteopicasso.it>
2020-02-03 21:05:24 +0100

    check

--- train.py
+++ train.py
@@ -1,19 +1,17 @@
 import sys
 from matplotlib import pyplot
 from keras.utils import to_categorical
-from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
 from keras.layers import Dense
 from keras.layers import Flatten
 from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
 
 #funzione che definisce il modello.
-def crea_modello():
-
+def define_model():
 	model = VGG16(include_top=False, input_shape=(224, 224, 3))
-	# rimuovo i layer predefiniti
+	# setto i layers precaricati come non utilizzabili
 	for layer in model.layers:
 		layer.trainable = False
 	# aggiungo layer per fare classificazione binarie
@@ -27,41 +25,24 @@ def crea_modello():
 	model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
 	return model
 
-
-# Funzione che stampa su file le curve di apprendimento
-def stampa(history):
-	# accuratezza
-	pyplot.subplot(212)
-	pyplot.title('Accuratezza nella classificazione')
-	pyplot.plot(history.history['accuracy'], color='blue', label='train')
-	pyplot.plot(history.history['val_accuracy'], color='red', label='test')
-	# salvo su file
-	filename = sys.argv[0].split('/')[-1]
-	pyplot.savefig(filename + '_plotTTED.png')
-	pyplot.close()
-
 # addestramento
-def addestra():
+def train():
 	# definisco il modello
-	model = crea_modello()
+	model = define_model()
 	# preparo sia per test che train i batch degli input.
 	datagen = ImageDataGenerator(featurewise_center=True)
-	# imposto i valori medi RGB del dataset
+	# valori medi centratura
 	#datagen.mean = [123.68, 116.779, 103.939]
 	# definisco su quali dati il modello apprenderà e su quali effettuerà i test
-	train_it = train_datagen.flow_from_directory('dataset_dogs_vs_cats/train/',
+	train_it = datagen.flow_from_directory('dataset_dogs_vs_cats/train/',
 		class_mode='binary', batch_size=64, target_size=(224, 224))
-	test_it = test_datagen.flow_from_directory('dataset_dogs_vs_cats/test/',
+	test_it = datagen.flow_from_directory('dataset_dogs_vs_cats/test/',
 		class_mode='binary', batch_size=64, target_size=(224, 224))
 	# ADDESTRO IL MODELLO
-	history = model.fit_generator(train_it, steps_per_epoch=len(train_it),
+	model.fit_generator(train_it, steps_per_epoch=len(train_it),
 		validation_data=test_it, validation_steps=len(test_it), epochs=10, verbose=1)
-	stampa(history)
-	model.save("vgg16.h5")
-	#model.save_weights('my_model_weights.h5')
+	#salvo
+	model.save('vgg16.h5')
+	#model.save_weights("pesi.h5")
 
-	#calcolo una valutazione sulla precisione del modello appena addestrato
-	_, acc = model.evaluate_generator(test_it, steps=len(test_it), verbose=0)
-	print('> %.3f' % (acc * 100.0))
-
-addestra()
+train()
